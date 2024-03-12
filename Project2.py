84e98d8f2cee81e15623a0294dc5de0b5cc5a8a4
VETRISELVI N <138320431+Vetriselvi-N@users.noreply.github.com>
2024-03-12 11:55:27 +0530

    Dissect the string

---
+++ Project2.py
@@ -0,0 +1,7 @@
+print("Welcome to Dissect that String!\n")
+str1= "Hello World!"
+print("Word Count: " + str(len(str1)))                  # use typecasting for the concatenation of int with str
+print("In upper case: " + str1.upper())
+print("In lower case: " +str1.lower() )
+print("The first letter of the string is: " + str1[0] )
+print("The last letter of the string is: " +str1[-1] )
